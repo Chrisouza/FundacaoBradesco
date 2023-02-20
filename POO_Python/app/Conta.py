0a95dc0f381a202ebcc53c05a2725a14b6a87a11
Richardson Christian <rizinhof5@gmail.com>
2023-02-20 12:54:14 -0300

    Módulo 3 Finalizado

--- POO_Python/app/Conta.py
+++ POO_Python/app/Conta.py
@@ -1,6 +1,17 @@
 class Conta:
 
     def __init__(self, titular, numero, saldo):
-        self.saldo = 0
-        self.numero = numero
-        self.titular = titular
+        self._saldo = 0
+        self._numero = numero
+        self._titular = titular
+
+    @property
+    def get_saldo(self):
+        return self.saldo
+
+    @get_saldo.setter
+    def set_saldo(self, saldo):
+        if (saldo < 0):
+            print("O saldo não pode ser negativo")
+        else:
+            self._saldo = saldo
--- POO_Python/app/Main.py
+++ POO_Python/app/Main.py
@@ -1,13 +1,14 @@
+from Conta import Conta
+from Cliente import Cliente
+
+
 class Main:
     pass
 
+
 print("Testando o projeto")
 
-from Cliente import Cliente
-
-from Conta import Conta
-
 c1 = Cliente("João", "114444-2222")
-conta = Conta(c1.nome, 6565, 0)
+conta = Conta(c1._nome, 6565, 0)
 
-print(conta.titular," Número: ",conta.numero," Seu saldo: ",conta.saldo)
+print(conta.titular, " Número: ", conta.numero, " Seu saldo: ", conta.saldo)
